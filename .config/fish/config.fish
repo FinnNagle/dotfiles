fcb716f3eba7a2955a1154c58885bd3fde444ab9
FinnNagle <jimmyscrambleskat@protonmail.com>
2022-04-21 21:05:43 -0500

    osx fish src added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,16 +6,33 @@ set fish_greeting ""
 
 set -gx TERM xterm-256color
 
-alias ll="exa --no-user --icons --long --header --git --sort=name --group-directories-first --bytes"
-alias lla="exa --no-user --icons --long --header --git --sort=name --group-directories-first --bytes --all"
-alias l="ll"
-alias la="lla"
-alias home="tmux attach -t home"
-alias ta="tmux attach -t "
-alias cdot="cd /Users/connor/Projects/dotfiles && pwd && la"
-alias cdotf="cd /Users/connor/Projects/dotfiles/.config && pwd && la"
+# theme
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+alias ta "tmux attach -t "
+alias home "tmux attach -t home"
+alias cdot "cd /Users/connor/Projects/dotfiles && pwd && la"
+alias cdotf "cd /Users/connor/Projects/dotfiles/.config && pwd && la"
 
 # starship init fish | source
+
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    source (dirname (status --current-filename))/config-linux.fish
+  case '*'
+    source (dirname (status --current-filename))/config-windows.fish
+end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
